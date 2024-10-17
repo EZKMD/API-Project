6dcf087033e25a102a427f3a9758c4be7abf3f63
EZKMD <emidhun92@gmail.com>
2024-10-17 14:06:50 +1030

    Function of conversion works, but no design or error data has been tested

--- js/script.js
+++ js/script.js
@@ -1,18 +1,56 @@
 
 
+
+//Interactables:
+
+const dropdownSelection = document.getElementById("dropdown");
+
+const calculateBtn = document.getElementById("calculate");
+
+const amountField = document.getElementById("amount-input");
+
+let outputField = document.getElementById("output");
+
+let currencyToConvert;
+let amountValue;
+
+dropdownSelection.addEventListener("change", () => {
+    currencyToConvert = dropdownSelection.value;
+    console.log(currencyToConvert);
+})
+
+calculateBtn.addEventListener("click", () => {
+    amountValue = amountField.value;
+    callAPI()   
+    
+})
+
+
 // Url form: https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}
 
 // Following is the latest exchange rates of the euro
-currencyQueryURL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+currencyQueryURL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`;
 // It returns a json of the equivalent of each currency for 1 Euro
 
+let conversionRate;
+let convertedAmount;
 
-$.ajax({
-    url: currencyQueryURL,
-    method: "GET"
-}).then(
-    (response) => {
-        console.log(response)
-    }
-)
+function callAPI(){
+    $.ajax({
+        url: currencyQueryURL,
+        method: "GET"
+    }).then(
+        (response) => {
 
+            //Checking types of variables
+            conversionRate = response.eur[currencyToConvert];
+            amountValue = parseInt(amountValue);
+            convertedAmount = (amountValue/conversionRate).toFixed(2);
+            console.log("Converted: "+convertedAmount);
+            outputField.textContent = convertedAmount;
+
+
+            
+        }
+    )
+}
