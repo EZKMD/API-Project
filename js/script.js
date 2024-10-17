24e6f5b8394db65b1b45a489429efaf657386096
EZKMD <emidhun92@gmail.com>
2024-10-17 15:18:28 +1030

    Multiple currency exhange

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ let currencySigns = {
 
 //Interactables:
 
-const dropdownSelection = document.getElementById("dropdown");
+const currencyToConvertSelection = document.getElementById("currencyToConvert");
+
+const currencyToConvertToSelection = document.getElementById("currencyToConvertTo")
 
 const calculateBtn = document.getElementById("calculate");
 
@@ -16,18 +18,22 @@ const amountField = document.getElementById("amount-input");
 
 let outputField = document.getElementById("output");
 
-let currencyToConvert;
 let amountValue;
+let currencyToConvert;
+let currencyToConvertTo;
 
-dropdownSelection.addEventListener("change", () => {
-    currencyToConvert = dropdownSelection.value;
-    console.log(currencyToConvert);
+currencyToConvertSelection.addEventListener("change", () => {
+    currencyToConvert = currencyToConvertSelection.value;
+})
+
+currencyToConvertToSelection.addEventListener("change", () => {
+    currencyToConvertTo = currencyToConvertToSelection.value;
 })
 
 calculateBtn.addEventListener("click", () => {
     amountValue = parseInt(amountField.value);
      if (!isNaN(amountValue) && currencyToConvert != undefined){
-        callAPI(amountValue, currencyToConvert)
+        callAPI(amountValue, currencyToConvert, currencyToConvertTo)
     } else{
         window.alert("Error");
     } 
@@ -35,16 +41,20 @@ calculateBtn.addEventListener("click", () => {
 })
 
 
+
 // Url form: https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}
 
 // Following is the latest exchange rates of the euro
-currencyQueryURL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`;
+
 // It returns a json of the equivalent of each currency for 1 Euro
 
 let conversionRate;
 let convertedAmount;
 
-function callAPI(amountValue, currencyToConvert){
+function callAPI(amountValue, currencyToConvert, currencyToConvertTo){
+    console.log(currencyToConvert)
+    console.log(currencyToConvertTo)
+    currencyQueryURL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/"+currencyToConvertTo+".json";
     $.ajax({
         url: currencyQueryURL,
         method: "GET"
@@ -52,8 +62,10 @@ function callAPI(amountValue, currencyToConvert){
         (response) => {
 
             //Checking types of variables
-            conversionRate = response.eur[currencyToConvert];
+            conversionRate = response[currencyToConvertTo][currencyToConvert];
             convertedAmount = (amountValue/conversionRate).toFixed(2);
+            console.log("Amount: "+amountValue)
+            console.log("Conversion Rate: "+conversionRate)
             console.log("Converted: "+convertedAmount);
             outputField.textContent = `€${convertedAmount}`
             
