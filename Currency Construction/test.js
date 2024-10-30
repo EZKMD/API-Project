32e62b7499c72b3acb171d4762466b8d0d0e739f
EZKMD <emidhun92@gmail.com>
2024-10-30 23:24:42 +1030

    Commenting

--- Currency Construction/test.js
+++ Currency Construction/test.js
@@ -1,4 +1,7 @@
 
+
+// Following is the model of a test to see if the following codes actually have a conversion rate within the API
+
 let currencyCodes = ['all', 'afn', 'ars', 'awg', 'aud', 'azn', 'bsd', 'bbd', 'byn', 'bzd', 'bmd', 'bob', 'bam', 'bwp', 'bgn', 'brl', 'bnd', 'khr', 'cad', 'kyd', 'clp', 'cny', 'cop', 'crc', 'hrk', 'cup', 'czk', 'dkk', 'dop', 'xcd', 'egp', 'svc', 'eur', 'fkp', 'fjd', 'ghs', 'gip', 'gtq', 'ggp', 'gyd', 'hnl', 'hkd', 'huf', 'isk', 'inr', 'idr', 'irr', 'imp', 'ils', 'jmd', 'jpy', 'jep', 'kzt', 'kpw', 'krw', 'kgs', 'lak', 'lbp', 'lrd', 'mkd', 'myr', 'mur', 'mxn', 'mnt', 'mzn', 'nad', 'npr', 'ang', 'nzd', 'nio', 'ngn', 'nok', 'omr', 'pkr', 'pab', 'pyg', 'pen', 'php', 'pln', 'qar', 'ron', 'rub', 'shp', 'sar', 'rsd', 'scr', 'sgd', 'sbd', 'sos', 'zar', 'lkr', 'sek', 'chf', 'srd', 'syp', 'twd', 'thb', 'ttd', 'try', 'tvd', 'uah', 'gbp', 'usd', 'uyu', 'uzs', 'vef', 'vnd', 'yer', 'zwd']
 const $ = require('jquery')(require('jsdom').jsdom().defaultView);
 
